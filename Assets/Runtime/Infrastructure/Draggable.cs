46acb2265dd03b2fecb5e0afe3bcecc9dbbfd3fd
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-26 11:26:27 +0100

    feat: objects now can be draggables

---
+++ Assets/Runtime/Infrastructure/Draggable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private Vector3 offset;
+    private bool isDragging = false;
+
+    void OnMouseDown()
+    {
+        offset = transform.position - GetMouseWorldPos();
+        isDragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = GetMouseWorldPos() + offset;
+        }
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
