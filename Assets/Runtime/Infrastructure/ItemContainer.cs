f393e20009bb4b0dbf529c5416dcdcb290c6407f
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-26 18:21:38 +0100

    feat: fix items to be shown

--- Assets/Runtime/Infrastructure/ItemContainer.cs
+++ Assets/Runtime/Infrastructure/ItemContainer.cs
@@ -1,4 +1,5 @@
 using Runtime.Application;
+using Runtime.ItemManagement.Application;
 using Runtime.ItemManagement.Domain;
 using UnityEngine;
 
@@ -9,5 +10,14 @@ namespace Runtime.Infrastructure
         [SerializeField] private Item item;
         public Sprite GetSprite => item.sprite;
         public Sprite Icon => UIResources.Grab.Icon;
+        
+        private void OnMouseOver()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                HandleInventory.AddItem(item.ID);
+                Destroy(gameObject);
+            }
+        }
     }
 }
