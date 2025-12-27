0b953a3c0c35bb305fc75c042f3ae4fe7844d268
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-28 00:33:35 +0100

    feat: water can be obtained from termo

---
+++ Assets/Runtime/Puzzles/TermoPuzzle.cs
@@ -0,0 +1,98 @@
+using Runtime.Application;
+using Runtime.Dialogues.Domain;
+using Runtime.ItemManagement.Application;
+using Runtime.ItemManagement.Domain;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Infrastructure
+{
+    public class TermoPuzzle:Interaction
+    {
+        [SerializeField] private string itemOnHand = "Cortapizzas";
+        [SerializeField] private DialogueData dialogueCompleted;
+        [SerializeField] private DialogueData dialogueNoItem;
+        [SerializeField] private DialogueData dialogueWrongItem;
+        
+        [SerializeField] private string itemOnHandTiGetWater = "EmptyGlass";
+        [SerializeField] private DialogueData dialogueWaterCompleted;
+        [SerializeField] private DialogueData dialogueWaterNoItem;
+        [SerializeField] private DialogueData dialogueWaterWrongItem;
+
+        [SerializeField] private SpriteRenderer pizzaRenderer;
+        [SerializeField] private SpriteRenderer boilerRenderer;
+        [SerializeField] private Sprite fullBoilerSprite;
+        [SerializeField] private Sprite emptyBoilerSprite;
+        
+        [Inject] private readonly Inventory _inventory;
+        [Inject] private HandleInventory _handleInventory;
+        [Inject] private readonly ShowDialogue _showDialogue;
+
+        private bool pizzasWasRemoved = false;
+
+        protected override void Awake()
+        {
+            boilerRenderer.sprite = fullBoilerSprite;
+            pizzaRenderer.enabled = true;
+        }
+        public override void Interact()
+        {
+            if (!Interactable) return;
+            CutPizza();
+            GetWater();
+        }
+
+        private void CutPizza()
+        {
+            if (pizzasWasRemoved) return;
+            if (_inventory.HasitemOnHand(itemOnHand))
+            {
+                RemovePizza();
+            }
+            else
+            {
+                if (_inventory.HasSomethingOnHand)
+                {
+                    _showDialogue.Start(dialogueWrongItem);
+                }
+                else
+                {
+                    _showDialogue.Start(dialogueNoItem);
+                }
+            }
+        }
+
+        private void GetWater()
+        {
+            if (!pizzasWasRemoved) return;
+            if (_inventory.HasitemOnHand(itemOnHandTiGetWater))
+            {
+                _handleInventory.RemoveItemOnHand();
+                _handleInventory.AddItem("GlassFullOfWater");
+                _showDialogue.Start(dialogueWaterCompleted);
+                boilerRenderer.sprite = emptyBoilerSprite;
+                Disable();
+            }
+            else
+            {
+                if (_inventory.HasSomethingOnHand)
+                {
+                    _showDialogue.Start(dialogueWaterWrongItem);
+                }
+                else
+                {
+                    _showDialogue.Start(dialogueWaterNoItem);
+                }
+            }
+            
+        }
+
+        private void RemovePizza()
+        {
+            if (pizzasWasRemoved) return;
+            pizzaRenderer.enabled = false;
+            _handleInventory.RemoveItemOnHand();
+            pizzasWasRemoved = true;
+        }
+    }
+}
