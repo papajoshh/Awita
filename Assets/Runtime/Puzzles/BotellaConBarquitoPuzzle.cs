68cc54dd4a23683df925702ca39c255c29519e6e
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-28 00:56:22 +0100

    feat: change compressed textures

---
+++ Assets/Runtime/Puzzles/BotellaConBarquitoPuzzle.cs
@@ -0,0 +1,106 @@
+using Runtime.Application;
+using Runtime.Dialogues.Domain;
+using Runtime.ItemManagement.Application;
+using Runtime.ItemManagement.Domain;
+using UnityEngine;
+using Zenject;
+
+namespace Runtime.Infrastructure
+{
+    public class BotellaConBarquitoPuzzle:Interaction
+    {
+        [SerializeField] private string itemOnHand = "Pincitas";
+        [SerializeField] private DialogueData dialogueCompleted;
+        [SerializeField] private DialogueData dialogueNoItem;
+        [SerializeField] private DialogueData dialogueWrongItem;
+        
+        [SerializeField] private string itemOnHandTiGetWater = "EmptyGlass";
+        [SerializeField] private DialogueData dialogueWaterCompleted;
+        [SerializeField] private DialogueData dialogueWaterNoItem;
+        [SerializeField] private DialogueData dialogueWaterWrongItem;
+        
+        [SerializeField] private Sprite[] barquitoStages;
+        [SerializeField] private SpriteRenderer botellaRenderer;
+        
+        [Inject] private readonly Inventory _inventory;
+        [Inject] private HandleInventory _handleInventory;
+        [Inject] private readonly ShowDialogue _showDialogue;
+
+        private int dismountings = 0;
+        private int dismountingsNeeded = 2;
+        private bool completelyDismounted = false;
+
+        protected override void Awake()
+        {
+            botellaRenderer.sprite = barquitoStages[0];
+        }
+        public override void Interact()
+        {
+            if (!Interactable) return;
+            CompletelyDismountShip();
+            GetWater();
+        }
+
+        private void CompletelyDismountShip()
+        {
+            if (completelyDismounted) return;
+            if (_inventory.HasitemOnHand(itemOnHand))
+            {
+                DismountShip();
+            }
+            else
+            {
+                if (_inventory.HasSomethingOnHand)
+                {
+                    _showDialogue.Start(dialogueWrongItem);
+                }
+                else
+                {
+                    _showDialogue.Start(dialogueNoItem);
+                }
+            }
+        }
+
+        private void GetWater()
+        {
+            if (!completelyDismounted) return;
+            if (_inventory.HasitemOnHand(itemOnHandTiGetWater))
+            {
+                _handleInventory.RemoveItemOnHand();
+                _handleInventory.AddItem("GlassFullOfWater");
+                _showDialogue.Start(dialogueWaterCompleted);
+                botellaRenderer.sprite = barquitoStages[barquitoStages.Length - 1];
+                Disable();
+            }
+            else
+            {
+                if (_inventory.HasSomethingOnHand)
+                {
+                    _showDialogue.Start(dialogueWaterWrongItem);
+                }
+                else
+                {
+                    _showDialogue.Start(dialogueWaterNoItem);
+                }
+            }
+            
+        }
+
+        private void DismountShip()
+        {
+            dismountings++;
+            botellaRenderer.sprite = barquitoStages[dismountings];
+
+            
+            if (dismountings == 1)
+            {
+                stick.SetActive(true);
+            }
+            else if (dismountings >= dismountingsNeeded && !completelyDismounted)
+            {
+                _handleInventory.RemoveItemOnHand();
+                completelyDismounted = true;
+            }
+        }
+    }
+}
