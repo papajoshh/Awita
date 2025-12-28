a706ac4a4a914b2d1303ee2f74558f8ff698c763
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-28 18:09:14 +0100

    feat: router can now be switched

---
+++ Assets/Runtime/Puzzles/TurnOnRouterInteraction.cs
@@ -0,0 +1,44 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Zenject;
+
+namespace Runtime.Infrastructure
+{
+    public class TurnOnRouterInteraction: Interaction
+    {
+        [SerializeField] private SpriteRenderer turnOnRouter;
+        [SerializeField] private SpriteRenderer turnOffRouter;
+        [SerializeField] private AudioClip _audioClip;
+
+        [Inject] private readonly AudioPlayer _audioPlayer;
+        [Inject] private readonly Router _router;
+        protected override void Awake()
+        {
+            base.Awake();
+            Close();
+        }
+
+        public override void Interact()
+        {
+            if (!Interactable) return;
+            Open();
+            Disable();
+        }
+        
+
+        private void Close()
+        {
+            turnOnRouter.color = new Color (1, 1, 1, 0);
+            turnOffRouter.color = Color.white;
+        }
+
+        private void Open()
+        {
+            _router.Connect();
+            turnOnRouter.DOColor(Color.white, 0.75f);
+            turnOffRouter.DOColor(new Color (1, 1, 1, 0), 0.75f);
+            _audioPlayer.PlaySfx(_audioClip, 0.2f);
+        }
+    }
+}
