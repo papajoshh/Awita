364babe92a9ee68b9c7cc35f4b1239a68437074a
Papajoshh <josecarlosromerodiaz@gmail.com>
2025-12-28 23:10:55 +0100

    fix: softblock drinking

--- Assets/PauseAnimation.cs
+++ Assets/PauseAnimation.cs
@@ -5,13 +5,29 @@ public class PauseAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     public event Action OnEnded;
+
+    private bool cacheResume;
     public void Pause()
     {
+        if (cacheResume)
+        {
+            _animator.speed = 1;
+            cacheResume = false;
+            return;
+        }
         _animator.speed = 0;
     }
 
     public void Resume()
     {
+        if (_animator.speed >= 1)
+        {
+            cacheResume = true;
+        }
+        else
+        {
+            _animator.speed = 1;
+        }
         _animator.speed = 1;
     }
 
