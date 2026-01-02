4fc8b805c7adac8acaaf08fc4ba1fc7bb108d168
VlaDOS <vladi095@gmail.com>
2026-01-02 19:58:26 +0100

    Feat: Better grifos (different text when not using the empty glass) & custom grifo for the bath

--- Assets/Runtime/Puzzles/ShowDialogueInteraction.cs
+++ Assets/Runtime/Puzzles/ShowDialogueInteraction.cs
@@ -1,5 +1,6 @@
 using Runtime.Application;
 using Runtime.Dialogues.Domain;
+using Runtime.ItemManagement.Domain;
 using UnityEngine;
 using Zenject;
 
@@ -8,12 +9,25 @@ namespace Runtime.Infrastructure
     public class ShowDialogueInteraction:Interaction
     {
         [SerializeField] private DialogueData _dialogue;
-        
+        [SerializeField] private DialogueData _dialogueWithGlass;
+        [SerializeField] private string itemOnHandToGetWater = "EmptyGlass";
+
         [Inject] private readonly ShowDialogue _showDialogue;
+        [Inject] private readonly Inventory _inventory;
+
         public override void Interact()
         {
             if(!Interactable) return;
-            _showDialogue.Start(_dialogue);
+
+            if (_inventory.HasitemOnHand(itemOnHandToGetWater))
+            {
+                _showDialogue.Start(_dialogueWithGlass);
+            }
+            else
+            {
+                _showDialogue.Start(_dialogue);
+            }
+           
         }
     }
 }
